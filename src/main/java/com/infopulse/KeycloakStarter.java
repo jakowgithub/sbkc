09c4f74e16125071b6dcc63227c5137ca9c59c58
galkin <oleksandr.galkin@intapp.com>
2018-12-15 14:38:11 +0200

    add keycloak

---
+++ src/main/java/com/infopulse/KeycloakStarter.java
@@ -0,0 +1,35 @@
+package com.infopulse;
+
+import com.infopulse.keycloak.KeycloakServerProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+@EnableConfigurationProperties(KeycloakServerProperties.class)
+public class KeycloakStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakStarter.class, args);
+    }
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties, KeycloakServerProperties keycloakServerProperties) {
+
+        return (evt) -> {
+
+            Integer port = serverProperties.getPort();
+            String rootContextPath = serverProperties.getServlet().getContextPath();
+            if(rootContextPath == null){
+                rootContextPath ="";
+            }
+            String keycloakContextPath = keycloakServerProperties.getContextPath();
+
+            System.out.printf("Embedded Keycloak started: http://localhost:%s%s%s to use keycloak%n", port, rootContextPath, keycloakContextPath);
+        };
+    }
+}
